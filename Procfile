856aec1524c3ffe5c511b3fcc30c5bfc68210717
louparker <lupaka91@gmail.com>
2021-04-19 20:36:04 +0000

    Set up heroku deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn myochlinn.wsgi:application
--- myochlinn/settings.py
+++ myochlinn/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = '@cpduy#4rp6ou^yv*kb&_+xv^wm-2m3$w%^&rq3*sl(uyhimc&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['myochlinn.herokuapp.com']
 
 
 # Application definition
@@ -126,12 +126,17 @@ WSGI_APPLICATION = 'myochlinn.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
